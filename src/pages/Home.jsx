3f62008fbccdfb19bbced45d0ba172d14c005a48
ReewHup <taweesak99924@gmail.com>
2025-08-01 02:10:56 +0700

    update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <>
             <section className='h-screen bg-cover bg-center flex items-center justify-between text-white' style={{ backgroundImage: 'url(https://images.pexels.com/photos/2668314/pexels-photo-2668314.jpeg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
                 <div className='container mx-auto p-4'>
-                    <h2 className='text-5xl font-bold mb-4 text-blue-950'>Welcome to the Card Gallery</h2>
+                    <h2 className='text-5xl font-bold mb-4 text-blue-950'>Welcome to the Card Gallery Na Ja</h2>
                     <p className='text-black mb-6 text-lg font-semibold'>Browse through our collection of cards and discover amazing designs.</p>
                     <Button nameButton="Explore Cards" />
                 </div>
